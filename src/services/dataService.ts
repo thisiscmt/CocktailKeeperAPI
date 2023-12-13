28eba2d62607f018f7a7e37900bb4a8f15550c0e
Chris Tybur <thisiscmt@gmail.com>
2023-12-12 22:16:49 -0800

    Assorted fixes

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -4,23 +4,16 @@ import fs from 'fs';
 import { User } from '../models/models.js';
 import { COCKTAIL_DATA_FILE } from '../constants/constants.js';
 
-// TODO: Switch these to use async versions of the fs functions
 export const getServerBackup = (user: User): Promise<string> => {
     return new Promise((resolve, reject) => {
         const dataFilePath = path.join(process.cwd(), 'app_data', user.id, COCKTAIL_DATA_FILE);
 
-        fs.stat(dataFilePath, (error) => {
+        fs.readFile(dataFilePath, { encoding: 'utf8' }, (error, data) => {
             if (error) {
                 reject(error);
             }
 
-            fs.readFile(dataFilePath, { encoding: 'utf8' }, (error, data) => {
-                if (error) {
-                    reject(error);
-                }
-
-                resolve(data);
-            });
+            resolve(data);
         });
     });
 };
@@ -29,25 +22,15 @@ export const createServerBackup = (user: User, cocktailDataJSON: string): Promis
     return new Promise((resolve, reject) => {
         const userPath = path.join(process.cwd(), 'app_data', user.id);
 
-        fs.stat(userPath, (error, stats) => {
-            if (error || !stats.isDirectory()) {
-                reject(error);
-            }
+        fs.mkdir(userPath, () => {
+            const dataFilePath = path.join(userPath, COCKTAIL_DATA_FILE);
 
-            fs.mkdir(userPath, (error) => {
+            fs.writeFile(dataFilePath, cocktailDataJSON, { encoding: 'utf8' }, (error) => {
                 if (error) {
                     reject(error);
                 }
 
-                const dataFilePath = path.join(userPath, COCKTAIL_DATA_FILE);
-
-                fs.writeFile(dataFilePath, cocktailDataJSON, { encoding: 'utf8' }, () => {
-                    if (error) {
-                        reject(error);
-                    }
-
-                    resolve();
-                });
+                resolve();
             });
         });
     });
